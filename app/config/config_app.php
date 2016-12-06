87a2dd408c3f100e9c08f56c3595b623e9863f1f
Dayze <elye24@gmail.com>
2016-12-06 20:33:58 +0100

    commit

--- app/config/config_app.php
+++ app/config/config_app.php
@@ -1,7 +1,2 @@
 <?php
 require("app/model/Tools.php");
-DEFINE("NAME_PROJECT", "mvcwm/");
-DEFINE("PROJECT_PATH", "/var/www/html/" . NAME_PROJECT);
-DEFINE("APP_PATH", "app/");
-DEFINE("RESSOURCE_PATH", "localhost/" . NAME_PROJECT . "vendor/public/");
-DEFINE("VIEW_PATH", PROJECT_PATH . APP_PATH . "views/");
